2a0798d5646ad034daaa5047a4288148103b2750
ugoCodePasBeaucoup <max.planck.bg@gmail.com>
2023-10-22 12:59:49 +0200

    préparation header liste chaînée générique

---
+++ src/structure/sdgenlklist.h
@@ -0,0 +1,28 @@
+/*-----------------------------------------------------------------*/
+/**
+    Projet REV party - Structure liste chaînée générique
+    \file sdgenlklist.h
+    \author : Ugo VALLAT
+    \date creation date : 22/10/2023
+ 
+ */
+/*-----------------------------------------------------------------*/
+
+/**
+ * @brief Cette librairie implémente une liste doublement chaînée générique
+ *
+ * Implémente la liste sous forme de noeuds et utilise une sentinelle
+ *
+ * @note haute performance en écriture( O(1) ) mais faible en lecture ( O(n))
+ *
+ */
+
+
+#ifndef __GEN_LK_LIST__H__
+#define __GEN_LK_LIST__H__
+
+#include <stdio.h>
+#include <stdbool.h>
+
+
+#endif
