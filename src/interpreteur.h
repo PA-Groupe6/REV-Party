d076f3c878b50124687e69a1d4646fb56d9de77b
Corentin <eritque@DESKTOP-4BHCV59>
2023-10-23 19:37:16 +0200

    creation du header

---
+++ src/interpreteur.h
@@ -0,0 +1,39 @@
+/** \file interpreteur.h
+ * \brief header de l'interpreteur de commande
+ * \author Ludwig Corentin
+*/
+
+
+typedef struct commande
+{
+    int module;         /*module appeler, cf voir dessous*/
+    int type_fichier;   /*type du fichier de vote, cf voir dessous*/
+    char *fichier;      /*nom du fichier de vote*/
+    char *log;          /*potentiel nom du fichier de log*/
+};
+
+/********
+* module *
+**********/
+
+#define UNI1 1
+#define UNI2 2
+#define MINIMAX 3
+#define RANGEMENT 4
+#define SCHULZE 5
+#define JUGEMENT_MAJORITAIRE 6
+#define ALL 7
+
+
+/*******************
+ * type de fichier *
+********************/
+
+#define BALLOT 9
+#define MATRICE_DUEL 1000000000000
+
+/************
+ * fonction *
+*************/
+
+commande intreprete(char* argv[]);
