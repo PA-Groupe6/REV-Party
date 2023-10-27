3f32b59c8bd20d2876a5789270293500badaf110
loferga <laforgemateo@gmail.com>
2023-10-27 16:08:39 +0200

    auxiliaire de test > > ajout de méthodes générales à l'utilitaire de test comprenant; > un StringBuilder > fonctions d'affichage d'érreur et de succès > constante pour couleurs d'affichage (standardisation des affichage)

--- test/test_utils.c
+++ test/test_utils.c
@@ -1,2 +1,67 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
+#include <malloc.h>
 #include "test_utils.h"
 
+#define OKC "\033[38;5;120m"
+#define KOC "\033[38;5;124m"
+
+StringBuilder* createStringBuilder() {
+    StringBuilder* string_builder = malloc(sizeof(struct s_string_builder));
+    string_builder->lastLine = 0;
+    return string_builder;
+}
+
+void emptyStringBuilder(StringBuilder* string_builder) {
+    string_builder->lastLine = 0;
+    string_builder->output[0] = '\0';
+}
+
+void addLineStringBuilder(StringBuilder* string_builder, char* line) {
+    strcpy(string_builder->output + string_builder->lastLine, line);
+    string_builder->lastLine+=strlen(line);
+}
+
+void printStringBuilder(StringBuilder* string_builder) {
+    printf("%s\n[>>>]: here\n", string_builder->output);
+}
+
+#define STATUS_LENGTH 67
+#define STATUS_PAD_CHAR '>'
+
+/**
+ * @date 27/10/2023
+ * @brief Affiche un statue formaté contenant un message
+ * 
+ * @param[in] message Message du statue
+ */
+void printStatus(const char* message) {
+    char status[STATUS_LENGTH];
+    int i = 0;
+    /* copie de message dans status */
+    for (; message[i] != '\0'; i++)
+        status[i] = message[i];
+    /* ajout d'un caractère de remplissage */
+    for (; i<STATUS_LENGTH-1; i++)
+        status[i] = STATUS_PAD_CHAR;
+    /* fin de la chaîne */
+    status[STATUS_LENGTH-1] = '\0';
+    printf("\n%s\n", status);
+}
+
+void printSuccess(char* test_name) {
+    /* taille du message sans test_name + taille test_name + '\0' */
+    int msg_size = 29 + strlen(test_name) + 1;
+    char message[msg_size];
+    sprintf(message, "(%sOK%s) %s passed ", OKC, RSTC, test_name);
+    printStatus(message);
+}
+
+void printFailure(char* test_name) {
+    /* taille du message sans test_name + taille test_name + '\0' */
+    int msg_size = 29 + strlen(test_name) + 1;
+    char message[msg_size];
+    sprintf(message, "(%sKO%s) %s passed ", KOC, RSTC, test_name);
+    printStatus(message);
+}
--- test/test_utils.h
+++ test/test_utils.h
@@ -1,4 +1,74 @@
 #ifndef __TEST_H__
 #define __TEST_H__
 
+/* Réinitialise la couleur de l'affichage */
+#define RSTC "\033[0m"
+/* Affiche en gras */
+#define BOLD "\033[1m"
+/* Couleur standard des fichiers sources */
+#define SRCC "\033[38;5;189m"
+/* Couleur standard des fichiers test */
+#define TSTC "\033[38;5;228m"
+/* Couleur d'erreur (même que gcc fatal error) */
+#define ERRC "\033[38;5;160m"
+
+/* Couleur standard d'annonce d'exécution */
+#define EXECC "\033[38;5;117m"
+/* Couleur de fond d'un succès */
+#define SUCCC "\033[48;5;2m"
+/* Couleur de fond d'un échec */
+#define FAILC "\033[48;5;124m"
+
+/**
+ * @date 26/10/2023
+ * @brief Construit une chaine de caractère multiligne par ajout successifs
+ * @note Ne pas oublier de vidé avec `emptyStringBuilder`
+ */
+typedef struct s_string_builder {
+    char output[2048];
+    int lastLine;
+} StringBuilder;
+
+/**
+ * @date 26/10/2023
+ * @brief Créer un StringBuilder
+ * @return Un pointer vers le StringBuilder créé
+ */
+StringBuilder* createStringBuilder();
+
+/**
+ * @date 26/10/2023
+ * @brief vide la chaîne construite jusqu'ici par un StringBuilder
+ * @param[in] string_builder Le StringBuilder à vider
+ */
+void emptyStringBuilder(StringBuilder* string_builder);
+
+/**
+ * @date 26/10/2023
+ * @brief Ajoute une ligne à un StringBuilder
+ * 
+ * @param[in] string_builder Le StringBuilder sur lequel faire l'ajout
+ * @param[in] line La ligne à ajouter
+ */
+void addLineStringBuilder(StringBuilder* string_builder, char* line);
+
+/**
+ * @date 26/10/2023
+ * @brief Affiche un StringBuilder dans la sortie standard
+ * @param string_builder Le StringBuilder à afficher
+ */
+void printStringBuilder(StringBuilder* string_builder);
+
+/**
+ * @date 26/10/2023
+ * @brief Affiche un message formaté de succès contenant le nom tu test passé
+*/
+void printSuccess(char* test_name);
+
+/**
+ * @date 26/10/2023
+ * @brief Affiche un message formaté d'échec contenant le nom tu test échoué
+*/
+void printFailure(char* test_name);
+
 #endif
