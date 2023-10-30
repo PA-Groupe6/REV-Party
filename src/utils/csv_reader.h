c652218abc419c5f550316647198108560efbc0d
Corentin <corentin.ludwig@outlook.fr>
2023-10-30 22:37:06 +0100

    csv_reader.h completion du header

--- src/utils/csv_reader.h
+++ src/utils/csv_reader.h
@@ -6,6 +6,30 @@
 #ifndef __CSV_READER__H__
 #define __CSV_READER__H__
 
-// TODO
+#include "duel.h"
+#include "bale.h"
+
+
+/**
+ * @date 30/10/2023
+ * @brief Remplit une matrice de duel appartir d'un fichier csv
+ * 
+ * @param[in] nom_file nom/path du fichier csv
+ * 
+ * @return une matrice @ref Duel
+*/
+Duel csvToDuel(char *file);
+
+
+/**
+ * @date 30/10/2023
+ * @brief Remplit un ballot appartir d'un fichier csv
+ * 
+ * @param[in] nom_file nom/path du fichier csv
+ * 
+ * @return une matrice @ref Bale
+*/
+Bale csvToBale(char *file);
+
 
 #endif
