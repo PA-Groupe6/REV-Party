266fef9539729811489a977d6b831b3ac3e94e39
ivaline <alina.iva.4@gmail.com>
2023-10-31 18:32:44 +0100

    condorcet.h
    header pour le module Condorcet, s'appuie implementation des duels

---
+++ test/test_interpreter.c
@@ -0,0 +1,51 @@
+#include <stdbool.h>
+#include "../src/interpreter.h"
+#include "test_utils.h"
+
+#define printsb(msg) addLineStringBuilder(string_builder, msg)
+
+StringBuilder* string_builder;
+int return_value;
+
+void beforeAll() {
+    string_builder = createStringBuilder();
+}
+
+void afterEach() {
+    emptyStringBuilder(string_builder);
+}
+
+void testInterprete() {
+    int argc;
+    char* cmd = "interprete";
+    char* file = "test/ressource/unit/d1.csv";
+    char* dflag = "-d";
+    char* iflag = "-i";
+    char* jflag = "-j";
+    char* oflag = "-o";
+    char* offlag = "test/ressource/unit/res.log";
+
+    printsb("\ntest sur \"interprete -d test/ressource/unit/d1.csv\"");
+    argc = 3;
+    char* argv_d[argc];
+
+    Command cmd = intreprete(3, argv_d);
+}
+
+void test_fun(bool(*f)(), int fnb, char* fname) {
+    bool test_success = f();
+    afterEach();
+    if (!test_success) {
+        return_value += fnb;
+        printFailure(fname);
+        printStringBuilder(string_builder);
+    } else printSuccess(fname);
+}
+
+int main() {
+    beforeAll();
+
+    test_fun(testInterprete, 1, "testInterprete");
+
+    return return_value;
+}
