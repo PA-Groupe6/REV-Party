bd5bdca3ededb30a037c1469240a29582266c43e
ivaline <alina.iva.4@gmail.com>
2023-10-31 18:32:44 +0100

    condorcet.h
    header pour le module Condorcet, s'appuie implementation des duels

--- src/module/condorcet.h
+++ src/module/condorcet.h
@@ -13,16 +13,17 @@
  * Ce fichier d'en-tête fournit les fonctions et les structures nécessaires pour calculer les résultats selon la méthode de Condorcet pour les données contenues dans duel.h.
  *
  *
+ * @note haute performance en lecture( O(1) ) mais faible en écriture ( O(n))
+ *
  * @remark En cas d'erreur, la variable errno est positionnée à la valeur appropriée, il est 
  * fortement recomandé de la vérifier surtout pour les fonctions ne renvoyant pas de pointeur
  */
 
-#ifndef __CONDORCET_H__
 #define __CONDORCET_H__
 #include <stdio.h>
 #include <stdbool.h>
 #include <errno.h>
-#include "../structure/duel.h"
+#include <duel.h>
 
 
 /*------------------------------------------------------------------*/
@@ -30,51 +31,74 @@
 /*------------------------------------------------------------------*/
 
 
+
 /**
  * @author IVANOVA ALina 
- * @date 04/11/2023
- * @brief on utilise la méthode vainqueur de Condorcet pour savoir qu'il existe le candidat gagnant
+ * @date 31/10/2023
+ * @brief on compare le candidat avec un autre pour trouver s'il a gagné ou non
+ *
+ * @param[in] duel matrcie des duels entre tous le candidats
+ * @param[in] numCandidat1 Le numéro du candidat dont nous comparons les votes avec un autre candidat
+ * @param[in] numCandidat2 Le numéro du candidat avec laquelle nous comparons
  * 
- * @param[in] duel matrice des duels entre tous le candidats
- *
- * @return Le nom du candidat gagnant
+ * @return boolean True si le candidat a gagné la duel avec un autre, sinon False
 */
-bool CondorcetWinnerCriterion(Duel* duel);
+bool hasWonTheDuel(Duel duel, int numCandidat1, int numCandidat2);
 
 
 /**
  * @author IVANOVA ALina 
- * @date 04/11/2023
- * @brief on utilise la méthode condorcet minimax pour trouver le candidat gagnant
+ * @date 31/10/2023
+ * @brief on calcule combien de fois le candidat a gagné dans tous ses duels
+ *
+ * @param[in] duel matrcie des duels entre tous le candidats
  * 
- * @param[in] duel matrice des duels entre tous le candidats
- *
- * @return Le nom du candidat gagnant
+ * @return nombre des duels gagné
 */
-char theWinnerMinimax(Duel* duel);
+int nbWins(Duel duel);
 
 
 /**
  * @author IVANOVA ALina 
- * @date 04/11/2023
- * @brief on utilise la méthode condorcet avec le rangement des paires par ordre décroissant pour trouver le candidat gagnant
+ * @date 31/10/2023
+ * @brief on trouve si le candidat a gagné dans le majorité des duels ou non
  *
- * @param[in] duel matrice des duels entre tous le candidats
- *
- * @return Le nom du candidat gagnant
+ * @param[in] duel matrcie des duels entre tous le candidats
+ * @param[in] numCandidat le numero du candidat pour trouver ses duels
+ * 
+ * @return boolean True si le candidat a gagné majorité des duel, sinon False
 */
-char theWinnerRankedPairs(Duel* duel);
+bool hasMostWinsInDuels(Duel duel, int numCandidat);
+
+
 
 /**
  * @author IVANOVA ALina 
- * @date 04/11/2023
- * @brief on utilise la méthode de Schulze pour trouver le candidat gagnant
+ * @date 31/10/2023
+ * @brief (Pour minimax) on calcule la perte maximale en trouvant la valeur maximale dans la rangée correspondante de la matrice. 
+ * La valeur maximale dans une rangée représente le scénario le plus défavorable pour ce candidat par rapport à tous les autres.
  *
- * @param[in] duel matrice des duels entre tous le candidats
- *
- * @return Le nom du candidat gagnant
+ * @param[in] duel matrcie des duels entre tous le candidats
+ * @param[in] numCandidat le numero du candidat pour trouver ses duels
+ * 
+ * @return la valeur maximale d'une perte dqns tous les duels
 */
-char theWinnerSchulze(Duel* duel);
+int maximumLoss(Duel duel, int numCandidat);
 
 
-#endif
+/**
+ * @author IVANOVA ALina 
+ * @date 31/10/2023
+ * @brief (Pour rangement des paires) On compare la force de chaque paire de candidats (Schwartz)
+ *
+ * @param[in] duel matrcie des duels entre tous le candidats
+ * @param[in] numCandidat le numero du candidat pour trouver ses duels
+ * 
+ * @return une liste de ces paires avec les marges (différence de préférence entre deux candidats dans une paire)
+*/
+List listWinningPairs(Duel duel);
+
+
+
+
+
