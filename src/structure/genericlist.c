a681faf6bc96cc9f6d6f9051dfabf9c0ab0994ab
ugoCodePasBeaucoup <max.planck.bg@gmail.com>
2023-11-01 18:33:46 +0100

    Modification gestion erreur + matrice
    warnl -> exitl
    ajout date + auteur dans *.c
    implémentation structure et itérateur matrice
    modification *Copy()
    ajout listSet() et genListSet()

--- src/structure/genericlist.c
+++ src/structure/genericlist.c
@@ -2,7 +2,6 @@
 /**
  * @file genericlist.c
  * @author VALLAT Ugo
- * @date 30/10/2023
  *
  * @brief Cette librairie implémente une liste générique pseudo statique d'entiers
  *
@@ -13,8 +12,8 @@
  *
  * @note haute performance en lecture( O(1) ) mais faible en écriture ( O(n))
  *
- * @remark En cas d'erreur, la variable errno est positionnée à la valeur appropriée, il est
- * fortement recomandé de la vérifier surtout pour les fonctions ne renvoyant pas de pointeur
+ * @remark En cas d'erreur, toutes les fonctions de list exit le progamme avec un
+ * message d'erreur
  */
 
 #include "genericlist.h"
@@ -29,7 +28,11 @@
 /*                     STRUCTURE LIST GENERIC                       */
 /*------------------------------------------------------------------*/
 
-/* Définition de la structure list*/
+/**
+ * @date  1/11/2023
+ * @author Ugo VALLAT
+ * @brief  Définition de la structure list
+ */
 struct s_gen_list {
     unsigned int memory_size; /* Taille du tableau en mémoire */
     unsigned int size;        /* taille de la liste (nombre éléments) */
@@ -37,29 +40,28 @@ struct s_gen_list {
 };
 
 /**
- * @brief Verifie si l'argument est NULL, si vrai position errno à EFAULT
- * et affiche un warning
+ * @date  1/11/2023
+ * @author Ugo VALLAT
+ * @brief Exit le programme avec message si arg == NULL
  * @param[in] arg argument à vérifier
  * @param[in] caller Nom de la fonction appelante
- * @return true si arg = NULL, false sinon
- *
  */
-bool testArgNull(void *arg, char *caller) {
-    if (arg == NULL) {
-        warnl("genericlist.c", "hisArgNull", "dans %s > erreur pointeur null", caller);
-        errno = EFAULT;
-        return true;
-    }
-    return false;
+void testArgNull(void *arg, char *caller) {
+    if (arg == NULL)
+        exitl("list.c", "hisArgNull", EXIT_FAILURE, "dans %s > erreur pointeur null", caller);
 }
 
+/**
+ * @date  1/11/2023
+ * @author Ugo VALLAT
+ */
 GenList *createGenList(unsigned int size) {
     GenList *l = malloc(sizeof(GenList));
     if (l == NULL)
         exitl("genericlist.c", "createGenList", EXIT_FAILURE, "erreur malloc list");
 
     l->tab = malloc(sizeof(void *) * size);
-    if (l->tab == NULL)
+    if (l->tab == NULL && size != 0)
         exitl("genericlist.c", "createGenList", EXIT_FAILURE, "erreur malloc tab");
 
     l->memory_size = size;
@@ -67,10 +69,14 @@ GenList *createGenList(unsigned int size) {
     return l;
 }
 
+/**
+ * @date  1/11/2023
+ * @author Ugo VALLAT
+ */
 void deleteGenList(ptrGenList *l) {
     /* test l != NULL */
-    if (testArgNull(l, "deleteGenList") || testArgNull((*l), "deleteGenList"))
-        return;
+    testArgNull(l, "deleteGenList");
+    testArgNull((*l), "deleteGenList");
 
     /* libération de la mémoire */
     free((*l)->tab);
@@ -79,6 +85,8 @@ void deleteGenList(ptrGenList *l) {
 }
 
 /**
+ * @date  1/11/2023
+ * @author Ugo VALLAT
  * @brief Modifie l'espace mémoire aloué au tableau
  *
  * @param l Pointeur vers la liste
@@ -86,6 +94,8 @@ void deleteGenList(ptrGenList *l) {
  * @pre l != NULL
  */
 void adjustMemorySize(GenList *l, unsigned int new_size) {
+    testArgNull(l, "adjustMemorySize");
+
     /* nouvelle taille de la liste */
     l->memory_size = new_size;
 
@@ -95,10 +105,13 @@ void adjustMemorySize(GenList *l, unsigned int new_size) {
         exitl("genericlist.c", "adjustMemorySize", EXIT_FAILURE, "echec realloc tab");
 }
 
-GenList *genListAdd(GenList *l, void *v) {
+/**
+ * @date  1/11/2023
+ * @author Ugo VALLAT
+ */
+void genListAdd(GenList *l, void *v) {
     /* test l != NULL */
-    if (testArgNull(l, "genListAdd"))
-        return NULL;
+    testArgNull(l, "genListAdd");
 
     /* agrandissement de la liste si pleine */
     if (l->size == l->memory_size)
@@ -107,18 +120,17 @@ GenList *genListAdd(GenList *l, void *v) {
     /* Ajout de la valeur */
     l->tab[l->size] = v;
     l->size++;
-    return l;
 }
 
-GenList *genListInsert(GenList *l, void *v, unsigned int i) {
+/**
+ * @date  1/11/2023
+ * @author Ugo VALLAT
+ */
+void genListInsert(GenList *l, void *v, unsigned int i) {
     /* vérification paramêtres */
-    if (testArgNull(l, "genListInsert"))
-        return NULL;
-    if (i > l->size) {
-        warnl("genericlist.c", "genListInsert", "position (%d) invalide", i);
-        errno = EINVAL;
-        return NULL;
-    }
+    testArgNull(l, "genListInsert");
+    if (i > l->size)
+        exitl("genericlist.c", "genListInsert", EXIT_FAILURE, "position (%d) invalide", i);
 
     /* agrandissement de la liste si pleine */
     if (l->size >= l->memory_size)
@@ -131,95 +143,109 @@ GenList *genListInsert(GenList *l, void *v, unsigned int i) {
     /* ajoute le nouvel élément */
     l->tab[i] = v;
     l->size++;
-
-    return l;
 }
 
-int genListPop(GenList *l) {
+/**
+ * @date  1/11/2023
+ * @author Ugo VALLAT
+ */
+void genListPop(GenList *l) {
     /* vérification paramêtre */
-    if (testArgNull(l, "listPop"))
-        return -1;
-    if (l->size <= 0) {
-        warnl("list.c", "listPop", "liste déjà vide");
-        errno = EPERM;
-        return -1;
-    }
+    testArgNull(l, "listPop");
+    if (l->size <= 0)
+        exitl("list.c", "listPop", EXIT_FAILURE, "liste déjà vide");
+
     /* suppression de l'élément */
     l->size--;
     adjustMemorySize(l, l->size);
-    return 0;
 }
 
-int genListRemove(GenList *l, unsigned int i) {
+/**
+ * @date  1/11/2023
+ * @author Ugo VALLAT
+ */
+void genListRemove(GenList *l, unsigned int i) {
     /* vérification paramêtres */
-    if (testArgNull(l, "genListRemove"))
-        return -1;
-    if (i >= l->size) {
-        warnl("genericlist.c", "genListRemove", "position (%d) invalide", i);
-        errno = EINVAL;
-        return -1;
-    }
+    testArgNull(l, "genListRemove");
+    if (i >= l->size)
+        exitl("genericlist.c", "genListRemove", EXIT_FAILURE, "position (%d) invalide", i);
+
     /* suppression de l'élément */
     for (int j = i; j < (int)l->size - 1; j++)
         l->tab[j] = l->tab[j + 1];
     l->size--;
     adjustMemorySize(l, l->size);
-    return 0;
 }
 
+/**
+ * @date  1/11/2023
+ * @author Ugo VALLAT
+ */
 bool genListEmpty(GenList *l) {
-    if (testArgNull(l, "listEmpty"))
-        return true;
+    testArgNull(l, "listEmpty");
     return l->size == 0;
 }
 
+/**
+ * @date  1/11/2023
+ * @author Ugo VALLAT
+ */
 unsigned int genListSize(GenList *l) {
-    if (testArgNull(l, "genListSize"))
-        return 0;
+    testArgNull(l, "genListSize");
     return l->size;
 }
 
+/**
+ * @date  1/11/2023
+ * @author Ugo VALLAT
+ */
 GenList *genListCopy(GenList *l) {
     /* vérification paramêtre */
-    if (testArgNull(l, "listCopy"))
-        return NULL;
+    testArgNull(l, "listCopy");
 
     /* création nouvelle liste */
     GenList *new = createGenList(l->size);
-    if (errno) {
-        warnl("genericlist.c", "genListCopy", "Echec création copie");
-        return NULL;
-    }
 
     /* copie des éléments */
-    for (unsigned int i = 0; i < l->size; i++) {
+    for (unsigned int i = 0; i < l->size; i++)
         genListAdd(new, l->tab[i]);
-        if (errno) {
-            warnl("genericlist.c", "genListCopy", "Echec copie éléments");
-            return NULL;
-        }
-    }
 
     return new;
 }
 
+/**
+ * @date  1/11/2023
+ * @author Ugo VALLAT
+ */
 void *genListGet(GenList *l, unsigned int i) {
     /* vérification paramêtre */
-    if (testArgNull(l, "genListGet"))
-        return NULL;
-    if (i >= l->size) {
-        warnl("genericlist.c", "genListGet", "position (%d) invalide", i);
-        errno = EINVAL;
-        return NULL;
-    }
+    testArgNull(l, "genListGet");
+    if (i >= l->size)
+        exitl("genericlist.c", "genListGet", EXIT_FAILURE, "position (%d) invalide", i);
 
     return l->tab[i];
 }
 
+/**
+ * @date  1/11/2023
+ * @author Ugo VALLAT
+ */
+void genListSet(GenList *l, void *v, unsigned int i) {
+    /* vérification paramêtre */
+    testArgNull(l, "genListSet");
+    if (i >= l->size)
+        exitl("genericlist.c", "genListSet", EXIT_FAILURE, "position (%d) invalide", i);
+
+    l->tab[i] = v;
+}
+
+/**
+ * @date  1/11/2023
+ * @author Ugo VALLAT
+ */
 void displayGenList(GenList *l) {
     /* vérification paramêtre */
-    if (testArgNull(l, "displayGenList"))
-        return;
+    testArgNull(l, "displayGenList");
 
     if (l->size == 0)
         printl("[ ]");
@@ -236,7 +262,11 @@ void displayGenList(GenList *l) {
 /*                         ITERATEUR                                */
 /*------------------------------------------------------------------*/
 
-/* Définition de la structure list_ite */
+/**
+ * @date  1/11/2023
+ * @author Ugo VALLAT
+ * @brief Définition de la structure list_ite
+ */
 struct s_gen_list_ite {
     GenList *list;              /* liste à parcourir */
     int cur;                    /* position actuelle */
@@ -244,13 +274,24 @@ struct s_gen_list_ite {
     bool next;                  /* appel à fonction next */
 };
 
+/**
+ * @date  1/11/2023
+ * @author Ugo VALLAT
+ */
 unsigned int next_forward(int i) { return i + 1; }
 
+/**
+ * @date  1/11/2023
+ * @author Ugo VALLAT
+ */
 unsigned int next_backward(int i) { return i - 1; }
 
+/**
+ * @date  1/11/2023
+ * @author Ugo VALLAT
+ */
 GenListIte *createGenListIte(GenList *l, int dir) {
-    if (testArgNull(l, "createGenListIte"))
-        return NULL;
+    testArgNull(l, "createGenListIte");
 
     /* création de l'itérateur */
     GenListIte *ite = malloc(sizeof(GenListIte));
@@ -270,46 +311,54 @@ GenListIte *createGenListIte(GenList *l, int dir) {
         ite->next = false;
         ite->cur = l->size;
     } else {
-        warnl("genericlist.c", "createGenListIte", "sens de parcours invalide");
+        exitl("genericlist.c", "createGenListIte", EXIT_FAILURE, "sens de parcours invalide");
         free(ite);
         return NULL;
     }
     return ite;
 }
 
+/**
+ * @date  1/11/2023
+ * @author Ugo VALLAT
+ */
 bool genListIteHasNext(GenListIte *ite) {
-    if (testArgNull(ite, "genListIteHasNext"))
-        return false;
+    testArgNull(ite, "genListIteHasNext");
     unsigned int next = ite->fnext(ite->cur);
     return (next < ite->list->size);
 }
 
-int genListIteNext(GenListIte *ite) {
-    if (testArgNull(ite, "genListIteNext"))
-        return -1;
-    if (!genListIteHasNext(ite)) {
-        warnl("genericlist.c", "genListIteNext", "aucun élément à lire");
-        return -1;
-    }
+/**
+ * @date  1/11/2023
+ * @author Ugo VALLAT
+ */
+void genListIteNext(GenListIte *ite) {
+    testArgNull(ite, "genListIteNext");
+    if (!genListIteHasNext(ite))
+        exitl("genericlist.c", "genListIteNext", EXIT_FAILURE, "aucun élément à lire");
 
     ite->cur = ite->fnext(ite->cur);
     ite->next = true;
-    return 0;
 }
 
+/**
+ * @date  1/11/2023
+ * @author Ugo VALLAT
+ */
 void *genListIteGetValue(GenListIte *ite) {
-    if (testArgNull(ite, "genListIteGetValue"))
-        return NULL;
-    if (!ite->next) {
-        warnl("genericlist.c", "genListIteGetValue", "lecture sans appel à next");
-        return NULL;
-    }
+    testArgNull(ite, "genListIteGetValue");
+    if (!ite->next)
+        exitl("genericlist.c", "genListIteGetValue", EXIT_FAILURE, "lecture sans appel à next");
+
     return genListGet(ite->list, ite->cur);
 }
 
+/**
+ * @date  1/11/2023
+ * @author Ugo VALLAT
+ */
 void deleteGenListIte(ptrGenListIte *ite) {
-    if (testArgNull(ite, "genListIteGetValue"))
-        return;
+    testArgNull(ite, "genListIteGetValue");
     free((*ite)->list);
     free((*ite));
     *ite = NULL;
@@ -321,10 +370,12 @@ void deleteGenListIte(ptrGenListIte *ite) {
 
 #ifdef DEBUG
 
+/**
+ * @date  1/11/2023
+ * @author Ugo VALLAT
+ */
 void printGenListLog(GenList *l) {
-    if (testArgNull(l, "printGenListLog"))
-        return;
-    char buff[2048];
+    testArgNull(l, "printGenListLog");
 
     printl("\n<+>------------[ genericlist.c ]-----------<+>\n\n");
     printl("[list.c] size = %d\n", l->size);
--- src/structure/matrix.h
+++ src/structure/matrix.h
@@ -1,7 +1,6 @@
 /**
  * @file matrix.h
  * @author VALLAT Ugo
- * @date  29/10/2023
  *
  * @brief Cette librairie implémente implémente une matrice d'entiers
  *
@@ -11,176 +10,147 @@
  * @note Implémente un nombre varié d'accesseur et d'opérateurs pour
  * à privilégiés pour de meilleures performances.
  *
- * @remark En cas d'erreur, la variable errno est positionnée à la valeur appropriée, il est 
- * fortement recomandé de la vérifier surtout pour les fonctions ne renvoyant pas de pointeur
+ * @remark En cas d'erreur, toutes les fonctions de la matrice exit le progamme avec un
+ * message d'erreur
  */
-
 
 #ifndef __MATRIX_H__
 #define __MATRIX_H__
-#include <stdio.h>
-#include <stdbool.h>
 #include <errno.h>
-
+#include <stdbool.h>
+#include <stdio.h>
 
 /*------------------------------------------------------------------*/
 /*                        STRUCTURE MATRIX                           */
 /*------------------------------------------------------------------*/
 
-
 /* Définition opaque de la structure Matrix */
 typedef struct s_matrix Matrix;
-typedef Matrix* ptrMatrix;
+typedef Matrix *ptrMatrix;
 
-
-
-/** 
+/**
  * @author VALLAT ugo
- * @date  29/10/2023
+ * @date  1/11/2023
  * @brief Crée une matrice de dimension fixe
  *
  * @param[in] nbl Nombre de lignes
  * @param[in] nbc Nombre de colonnes
  * @param[in] default_value valeur par défault à mettre dans les case (correspond à une case vide)
  *
- * @return pointeur vers la matrice, NULL si erreur
-*/
-Matrix* createMatrix(int nbl, int nbc, int default_value);
+ * @return pointeur vers la matrice
+ */
+Matrix *createMatrix(unsigned int nbl, unsigned int nbc, int default_value);
 
-
-/** 
+/**
  * @author VALLAT ugo
- * @date  29/10/2023
- * @brief Supprime la matrice et ses données et libère la mémoire 
- * 
+ * @date  1/11/2023
+ * @brief Supprime la matrice et ses données et libère la mémoire
+ *
  * @param[in] m matrice à supprimer
-*/
-void deleteMatrix(ptrMatrix* m);
+ */
+void deleteMatrix(ptrMatrix *m);
 
-
-/** 
+/**
  * @author VALLAT ugo
- * @date  29/10/2023
- * @brief Ajoute un élément dans la matrice à la position (l,c)
+ * @date  31/10/2023
+ * @brief Positionne un élément dans la matrice à l'emplacement (l,c)
  *
  * @param[in] m Pointeur vers la matrice
  * @param[in] l Ligne de destination (l,c)
  * @param[in] c Colonne de destination (l,c)
  * @param[in] v valeur à ajouter
  *
- * @pre (0,0) <= (l,c) < (nb_lignes, nb_colonnes)
- *
- * @return Adresse de la matrice, Null si erreur
-*/
-Matrix* matrixAdd(Matrix* m, unsigned int l, unsigned int c, int v);
+ * @pre (l,c) < (nb_lignes, nb_colonnes)
+ */
+void matrixSet(Matrix *m, unsigned int l, unsigned int c, int v);
 
-
-/** 
+/**
  * @author VALLAT ugo
- * @date  29/10/2023
- * @brief Supprime la valeur en position (l,c) et la remplace par la valeur default_value
- * 
+ * @date  31/10/2023
+ * @brief Efface la valeur en position (l,c) et la remplace par la valeur default_value
+ *
  * @param[in] m pointeur vers la matrice
  * @param[in] l Ligne de la suppression (l,c)
  * @param[in] c Colonne de la suppression(l,c)
- * 
- * @pre (0,0) <= (l,c) < (nb_lignes, nb_colonnes)
  *
- * @return -1 if error, 0 else
-*/
-int matrixRemove(Matrix* m, unsigned int l, unsigned int c);
-
+ * @pre (l,c) < (nb_lignes, nb_colonnes)
+ */
+void matrixErase(Matrix *m, unsigned int l, unsigned int c);
 
 /**
  * @author VALLAT ugo
- * @date  29/10/2023
+ * @date  1/11/2023
  * @brief Renvoie la valeur à la position (l,c) dans la matrice
- * 
+ *
  * @param[in] m Pointeur vers la matrice
  * @param[in] l Ligne de la valeur
  * @param[in] c Colonne de la valeur
- * 
- * @pre (0,0) <= (l,c) < (nb_lignes, nb_colonnes)
- * 
- * @return la valeur en position (l,c), -1 si erreur
+ *
+ * @pre (l,c) < (nb_lignes, nb_colonnes)
+ *
+ * @return la valeur en position (l,c)
  */
-int matrixGet(Matrix* m, unsigned int l, unsigned int c);
-
+int matrixGet(Matrix *m, unsigned int l, unsigned int c);
 
 /**
  * @author VALLAT ugo
- * @date  29/10/2023
+ * @date  1/11/2023
  * @brief Renvoie les dimension de la matrice sous la forme [nb_lignes, nb_colonnes]
- * 
+ *
  * @param[in] m Pointeur vers la matrice
- * 
- * @return tableau d'entiers naturels de taille 2 : [nombre_lignes, nombre_colonnes], 
- * NULL si erreur
+ *
+ * @return tableau d'entiers naturels de taille 2 : [nombre_lignes, nombre_colonnes]
  */
 
-unsigned int* matrixShape(Matrix* m);
+unsigned int *matrixShape(Matrix *m);
 
-
-/** 
+/**
  * @author VALLAT ugo
- * @date  29/10/2023
+ * @date  1/11/2023
  * @brief Ajouter une ligne à la position l
  *
  * @param[in] m Pointeur vers l'adresse de la matrice
  * @param[in] l Position de la ligne à ajouter
  *
  * @pre l <= nb_lignes
- *
- * @return Adresse de la matrice, Null si erreur
-*/
-Matrix* matrixInsertLine(ptrMatrix* m, unsigned int l);
+ */
+void matrixInsertLine(Matrix *m, unsigned int l);
 
-
-/** 
+/**
  * @author VALLAT ugo
- * @date  29/10/2023
+ * @date  1/11/2023
  * @brief Supprime la ligne l
  *
  * @param[in] m Pointeur vers l'adresse de la matrice
  * @param[in] l Position de la ligne à supprimer
  *
  * @pre l < nb_lignes
- *
- * @return Adresse de la matrice, Null si erreur
-*/
-Matrix* matrixRemoveLine(ptrMatrix* m, unsigned int l);
+ */
+void matrixRemoveLine(Matrix *m, unsigned int l);
 
-
-/** 
+/**
  * @author VALLAT ugo
- * @date  29/10/2023
+ * @date  1/11/2023
  * @brief Ajouter une ligne à la position c
  *
  * @param[in] m Pointeur vers l'adresse de la matrice
  * @param[in] l Position de la ligne à ajouter
  *
  * @pre c <= nb_colonnes
- *
- * @return Adresse de la matrice, Null si erreur
-*/
-Matrix* matrixInsertColumn(ptrMatrix* m, unsigned int c);
+ */
+void matrixInsertColumn(Matrix *m, unsigned int c);
 
-
-/** 
+/**
  * @author VALLAT ugo
- * @date  29/10/2023
+ * @date  1/11/2023
  * @brief Supprime la colonne c
  *
  * @param[in] m Pointeur vers l'adresse de la matrice
  * @param[in] l Position de la colonne à supprimer
  *
  * @pre c < nb_colonnes
- *
- * @return Adresse de la matrice, Null si erreur
-*/
-Matrix* matrixRemoveColumn(ptrMatrix* m, unsigned int c);
-
-
+ */
+void matrixRemoveColumn(Matrix *m, unsigned int c);
 
 /*------------------------------------------------------------------*/
 /*                          ITERATEUR                               */
@@ -188,168 +158,152 @@ Matrix* matrixRemoveColumn(ptrMatrix* m, unsigned int c);
 
 /* Définition opaque de la structure MatrixIte */
 typedef struct s_matrixIte MatrixIte;
-typedef struct s_matrixIte* ptrMatrixIte ;
-
+typedef struct s_matrixIte *ptrMatrixIte;
 
 /**
  * @author VALLAT ugo
- * @date 29/10/2023
+ * @date 1/11/2023
  * @brief Définie le type des fonctions prise en paramêtre de l'itérateur
- * 
+ *
  * @param[in] v Valeur de la case courante
  * @param[in] l Ligne courante
  * @param[in] c Colonne courante
  * @param[in] buff pointeur vers un buffer pouvant stocker un résultat
- * 
+ *
  * @return nouvelle valeur de la case
  **/
-typedef int (*fun_ite) (int v, unsigned int l, unsigned int c, void* buff);
-
-
+typedef int (*fun_ite)(int v, unsigned int l, unsigned int c, void *buff);
 
 /**
  * @author VALLAT ugo
- * @date 29/10/2023
- * @brief Crée un itérateur sur la matrice passée en entrée et le positionne 
+ * @date 1/11/2023
+ * @brief Crée un itérateur sur la matrice passée en entrée et le positionne
  * avant le premier élément
- * 
+ *
  * @param[in] m Pointeur vers la matrice à parcourir
  * @param[in] l Ligne à parcourir (-1 pour parcourir toutes les lignes)
  * @param[in] c Colonne à parcourir (-1 pour parcourir toutes les colonnes)
  * @param[in] fun Fonction à appliquer à chaque élément, si NULL aucun traitement
  * n'est appliqué et la matrice n'est pas modifiée
  * @param[in] buff pointeur vers le buffer fourni à fun
- * 
- * @return pointeur vers l'itérateur, NULL si erreur
+ *
+ * @return pointeur vers l'itérateur
+ *
+ * @note la taille de la matrice ne doit pas être modifiée durant l'utilisation de
+ * l'itérateur
+ * @note Parcours les lignes et colonne par ordre croissant : (0,0) -> (0,1) -> ... -> (1,0) -> ...
  **/
-MatrixIte* createMatrixIte(Matrix* m, unsigned int l, unsigned int c, fun_ite fun, void* buff);
-
-
+MatrixIte *createMatrixIte(Matrix *m, int l, int c, fun_ite fun, void *buff);
 
 /**
  * @author VALLAT ugo
- * @date 29/10/2023
+ * @date 1/11/2023
  * @brief Renvoie si il reste des éléments à parcourir
- * 
+ *
  * @param[in] ite pointeur vers l'itérateur
- * 
+ *
  * @return true si il reste des éléments, sinon false
  **/
-bool matrixIteHasNext(MatrixIte* ite);
+bool matrixIteHasNext(MatrixIte *ite);
 
 /**
  * @author VALLAT ugo
- * @date 29/10/2023
+ * @date 1/11/2023
  * @brief Décale l'itérateur sur le prochain élément, renvoie sa valeur avant modification,
  * applique la fonction fun et remplace la valeur par l'élément de retour de fun
- * 
- * @param[in] ite pointeur vers l'itérateur
- * 
- * @pre doit avoir un prochain élément 
  *
- * @return valeur avant modification, -1 si erreur
+ * @param[in] ite pointeur vers l'itérateur
+ *
+ * @pre doit avoir un prochain élément
+ *
+ * @return valeur avant modification
  */
-int matrixIteNext(MatrixIte* ite);
-
+int matrixIteNext(MatrixIte *ite);
 
 /**
  * @author VALLAT ugo
- * @date 29/10/2023
+ * @date 1/11/2023
  * @brief Renvoie la valeur courrante après application de la fonction fun
- * 
+ *
  * @param[in] ite pointeur vers l'itérateur
  *
  * @pre doit être sur un élément
- * 
- * @return élément courant, -1 si erreur
+ *
+ * @return élément courant
  */
-int matrixIteGetValue(MatrixIte* ite);
-
+int matrixIteGetValue(MatrixIte *ite);
 
 /**
  * @author VALLAT ugo
- * @date 29/10/2023
+ * @date 1/11/2023
  * @brief Supprime l'itérateur et libère la mémoire
- * 
+ *
  * @param[in] ite pointeur vers l'itérateur
  **/
-void deleteMatrixIte(ptrMatrixIte* ite);
-
-
-
-
-
+void deleteMatrixIte(ptrMatrixIte *ite);
 
 /*------------------------------------------------------------------*/
 /*                            UTILS                                 */
 /*------------------------------------------------------------------*/
 
-
 /**
  * @author VALLAT ugo
- * @date 29/10/2023
+ * @date 1/11/2023
  * @brief Applique la fonction fun à tous les éléments spédifiés de la matrice
- * 
+ *
  * @param[in] m Pointeur vers la matrice à parcourir
  * @param[in] l Ligne à parcourir (-1 pour parcourir toutes les lignes)
  * @param[in] c Colonne à parcourir (-1 pour parcourir toutes les colonnes)
  * @param[in] fun Fonction à appliquer à chaque élément, si NULL aucun traitement
  * n'est appliqué et la matrice n'est pas modifiée
  * @param[in] buff pointeur vers le buffer fourni à fun
- * 
- * @return -1 si erreur, 0 sinon
  **/
-int matrixMap(Matrix* m, unsigned int l, unsigned int c, fun_ite fun, void* buff);
-
+void matrixMap(Matrix *m, unsigned int l, unsigned int c, fun_ite fun, void *buff);
 
 /**
  * @author VALLAT ugo
- * @date 29/10/2023
+ * @date 1/11/2023
  * @brief Renvoie la somme des élément de la matrice, d'une ligne ou d'une colonne
- * 
+ *
  * @param[in] m Pointeur vers la matrice
  * @param[in] l Ligne cible (-1 pour toutes les lignes)
  * @param[in] c Colonne cible (-1 pour toutes les colonnes)
  *
- * @return somme des éléments, -1 si erreur
+ * @return somme des éléments
  **/
-int matrixSom(Matrix* m, unsigned int l, unsigned int c);
-
+int matrixSom(Matrix *m, unsigned int l, unsigned int c);
 
 /**
  * @author VALLAT ugo
- * @date 29/10/2023
+ * @date 1/11/2023
  * @brief Renvoie la plus grande valeur de la matrice, d'une ligne ou d'une colonne
- * 
+ *
  * @param[in] m Pointeur vers la matrice
  * @param[in] l Ligne cible (-1 pour toutes les lignes)
  * @param[in] c Colonne cible (-1 pour toutes les colonnes)
- * 
- * @return Tableau d'entiers de taille 3 contenant [valeur_max, ligne, colonne], NULL
- * si erreur
+ *
+ * @return Tableau d'entiers de taille 3 contenant [valeur_max, ligne, colonne]
  **/
-int* matrixMax(Matrix* m, unsigned int l, unsigned int c);
+int *matrixMax(Matrix *m, unsigned int l, unsigned int c);
 
 /**
  * @author VALLAT ugo
- * @date29/10/2023
+ * @date1/11/2023
  * @brief Renvoie la plus petite valeur de la matrice, d'une ligne ou d'une colonne
- * 
+ *
  * @param[in] m Pointeur vers la matrice
  * @param[in] l Ligne cible (-1 pour toutes les lignes)
  * @param[in] c Colonne cible (-1 pour toutes les colonnes)
- * 
- * @return Tableau d'entiers de taille 3 contenant [valeur_min, ligne, colonne], NULL
- * si erreur
+ *
+ * @return Tableau d'entiers de taille 3 contenant [valeur_min, ligne, colonne]
  **/
-int* matrixMin(Matrix* m, unsigned int l, unsigned int c);
-
+int *matrixMin(Matrix *m, unsigned int l, unsigned int c);
 
 /**
  * @author VALLAT ugo
- * @date 29/10/2023
+ * @date 1/11/2023
  * @brief Définie le type des fonctions prise en paramêtre du filter
- * 
+ *
  * @param[in] v Valeur de la case courante
  * @param[in] l Ligne courante
  * @param[in] c Colonne courante
@@ -360,48 +314,38 @@ typedef bool (*fun_filter)(int v, unsigned int l, unsigned int c);
 
 /**
  * @author VALLAT ugo
- * @date 29/10/2023
+ * @date 1/11/2023
  * @brief Filtre la matrice et renvoie une copie de la matrice avec uniquement
  * les éléments tel que fun(elem) = true
- * 
+ *
  * @param[in] m Pointeur vers la matrice
  * @param[in] fun Fonction de filtrage de type fun_filter
- * 
- * @return Pointeur vers la copie filtrée, NULL si erreur
+ *
+ * @return Pointeur vers la copie filtrée
  **/
-Matrix* matrixFilter(Matrix* m, fun_filter fun);
-
+Matrix *matrixFilter(Matrix *m, fun_filter fun);
 
 /**
  * @author VALLAT ugo
- * @date 29/10/2023
- * @brief Copie une matrice dans une autre de dimension égale ou supérieure
- * 
- * @param[in] matrix_src Pointeur vers la matrice source (à copier)
- * @param[in] matrix_dest Pointeur vers la matrice de destination (copie)
- * 
+ * @date 1/11/2023
+ * @brief Copie une matrice
+ *
+ * @param[in] m Pointeur vers la matrice source (à copier)
+ *
  * @pre (l_src, c_src) <= (l_dest,c_dest)
- * 
- * @return -1 si erreur, 0 sinon
+ * @return Pointeur vers la copie
  */
 
-int matrixCopy(Matrix* matrix_src, Matrix* matrix_dest);
-
-
+Matrix *matrixCopy(Matrix *m);
 
 /**
  * @author VALLAT ugo
- * @date 29/10/2023
+ * @date 1/11/2023
  * @brief Afficher la matrice dans la sortie standard stdout
- * 
+ *
  * @param[in] m Matrice à afficher
  */
-void displayMatrix(Matrix* m);
-
-
-
-
-
+void displayMatrix(Matrix *m);
 
 /*------------------------------------------------------------------*/
 /*                              DEBUG                               */
@@ -411,12 +355,12 @@ void displayMatrix(Matrix* m);
 
 /**
  * @brief Affiche dans le logger toutes les informations sur la matrice
- * 
+ *
  * @param[in]  m matrice à logger
  * @author VALLAT ugo
- * @date 29/10/2023
+ * @date 1/11/2023
  */
-void printMatrixLog(Matrix* m);
+void printMatrixLog(Matrix *m);
 
 #endif
 
