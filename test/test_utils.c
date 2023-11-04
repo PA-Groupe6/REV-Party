e82bbfd8ab24949f2c167259e01f7f35faeee22c
loferga <laforgemateo@gmail.com>
2023-11-04 22:21:03 +0100

    Auxiliaire
    implémentation d'une suppression de String Builder pour la sûreté de la mémoire

--- test/test_utils.c
+++ test/test_utils.c
@@ -41,6 +41,11 @@ void deleteStringBuilder(ptrStringBuilder* string_builder) {
     string_builder = NULL;
 }
 
+void deleteStringBuilder(ptrStringBuilder* string_builder) {
+    free((*string_builder)->output);
+    string_builder = NULL;
+}
+
 #define STATUS_LENGTH 67
 #define STATUS_PAD_CHAR '>'
 
