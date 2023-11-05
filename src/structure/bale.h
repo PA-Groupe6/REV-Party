0f6de0f71a37cadffa01cc7294ceb033cc0b63b6
ugoCodePasBeaucoup <max.planck.bg@gmail.com>
2023-11-05 19:51:53 +0100

    graph.h
    Ecriture header graph
    Correction documentation headers
    Correction fonctions headers

--- src/structure/bale.h
+++ src/structure/bale.h
@@ -12,6 +12,8 @@
  * Le ballot est de taille fixe et ses éléments ne sont pas modifiables après avoir été chargés
  * dans le ballot.
  *
+ * @note Taille maximum des étiquetées = @ref MAX_LENGHT_LABEL
+ *
  * @remark Les dimensions d'un ballot sont fixes
  *
  * @remark En cas d'erreur, toutes les fonctions du ballot exit le progamme avec un
@@ -20,10 +22,10 @@
 /*-----------------------------------------------------------------*/
 #ifndef __BALE_H__
 #define __BALE_H__
+#include "genericlist.h"
 #include <errno.h>
 #include <stdbool.h>
 #include <stdio.h>
-
 /**
  * @def DEFAULT_VALUE
  * @brief valeur par défaut d'une case
@@ -36,37 +38,37 @@
 /*------------------------------------------------------------------*/
 
 /**
- * @author VALLAT ugo
- * @date 1/11/2023
+ * @date 5/11/2023
  * @brief Définition opaque de la structure Bale
  */
 typedef struct s_bale Bale;
 typedef Bale *ptrBale;
 
 /**
- * @author VALLAT ugo
- * @date  1/11/2023
+ * @date  5/11/2023
  * @brief Crée un ballot
  *
  * @param[in] nbl Nombre de lignes de données (hors étiquettes)
  * @param[in] nbc Nombre de colonnes de données (hors étiquettes)
+ * @param[in] labels Liste générique des labels (liste de char*)
+ * @pre genericListSize(labels) == nbc && Forall x in labels, typeof(x) == char*
  *
  * @return pointeur vers le ballot
+ * @post Les labels sont ajoutés au colonne dans le même ordre que la liste
  */
-Bale *createBale(unsigned int nbl, unsigned int nbc);
+Bale *createBale(unsigned int nbl, unsigned int nbc, GenList *labels);
 
 /**
- * @author VALLAT ugo
- * @date 1/11/2023
+ * @date 5/11/2023
  * @brief Supprime le ballot et ses données en libérant la mémoire
  *
  * @param[in] b pointeur vers le ballot à supprimer
+ * @pre b != NULL
  */
 void deleteBale(ptrBale *b);
 
 /**
- * @author VALLAT ugo
- * @date 1/11/2023
+ * @date 5/11/2023
  * @brief Ajoute un élément dans le ballot à la position (l,c), l'élément
  * ne pourra plus être modifié
  *
@@ -75,89 +77,89 @@ void deleteBale(ptrBale *b);
  * @param[in] c Colonne de destination
  * @param[in] v valeur à ajouter
  * @pre l < nb_ligne && c < nb_colonnes
+ * @pre b != NULL
  *
  * @return Adresse du ballot
  */
 Bale *baleSetValue(Bale *b, unsigned int l, unsigned int c, int v);
 
 /**
- * @author VALLAT ugo
- * @date 1/11/2023
+ * @date 5/11/2023
  * @brief Renvoie la valeur à la position (l,c) dans le ballot
  *
  * @param[in] b Ballot à lire
  * @param[in] l Ligne de la valeur
  * @param[in] c Colonne de la valeur
  * @pre l < nb_ligne && c < nb_colonnes
+ * @pre b != NULL
  *
  * @return la valeur en position (l, c)
  */
-int baleGet(Bale *b, unsigned int l, unsigned int c);
+int baleGetValue(Bale *b, unsigned int l, unsigned int c);
 
 /**
- * @author VALLAT ugo
- * @date 1/11/2023
+ * @date 5/11/2023
  * @brief Renvoie les dimension du ballot sous la forme d'un couple (nb_lignes, nb_colonnes)
  *
  * @param[in] b Ballot à utiliser
+ * @pre b != NULL
  *
  * @return tableau d'entier de taille 2 : [nombre_lignes, nombre_colonnes]
  */
 unsigned int *baleShape(Bale *b);
 
 /**
- * @author VALLAT ugo
- * @date 1/11/2023
- * @brief Met une étiquette à une colonne
- * @note Par défaut l'étiquette vaut NULL
- *
- * @param[in] b Ballot à modifier
- * @param[in] c Indice de la colonne
- * @param[in] label Etiquette
- * @pre c < nb_colonnes
- */
-void baleSetLabel(Bale *b, unsigned int c, char *label);
-
-/**
- * @author VALLAT ugo
- * @date 1/11/2023
+ * @date 5/11/2023
  * @brief Renvoie le numéro de la colonne associée à l'étiquette
  *
  * @param[in] b Ballot à utiliser
  * @param[in] label  Etiquette de la colonne recherchée
+ * @pre b != NULL
+ * @pre Exist label
  *
  * @return numéro de la colonne
  */
-int baleGetColumn(Bale *b, char *label);
+int baleLabelToColumn(Bale *b, char *label);
+
+/**
+ * @date 5/11/2023
+ * @brief Renvoie le label associé à la colonne
+ *
+ * @param[in] b Ballot à utiliser
+ * @param[in] c numéro de la colonne
+ * @pre b != NULL
+ * @pre c < nb_colonnes
+ *
+ * @return label
+ */
+char *baleColumnToLabel(Bale *b, unsigned int c);
 
 /*------------------------------------------------------------------*/
 /*                          ITERATEUR                               */
 /*------------------------------------------------------------------*/
 
 /**
- * @author VALLAT ugo
- * @date 1/11/2023
+ * @date 5/11/2023
  * @brief Définition opaque de la structure BaleIte
  */
 typedef struct s_baleIte BaleIte;
 typedef struct s_baleIte *ptrBaleIte;
 
 /**
- * @author VALLAT ugo
- * @date 1/11/2023
+ * @date 5/11/2023
  * @brief Définie le type des fonctions prise en paramêtre de l'itérateur
  *
  * @param[in] v Valeur de la case courante
  * @param[in] l Ligne courante
  * @param[in] c Colonne courante
  * @param[in] buff pointeur vers un buffer pouvant stocker un résultat
+ * @pre l < nb_ligne && c < nb_colonnes
  *
  */
 typedef void (*fun_ite_bale)(int v, unsigned int l, unsigned int c, void *buff);
 
 /**
- * @author VALLAT ugo
- * @date 1/11/2023
+ * @date 5/11/2023
  * @brief Crée un itérateur sur le ballot passé en entrée et le positionne
  * avant le premier élément
  *
@@ -167,29 +169,31 @@ typedef void (*fun_ite_bale)(int v, unsigned int l, unsigned int c, void *buff);
  * @param[in] fun Fonction à appliquer à chaque élément sans modification du ballot,
  * si NULL aucun traitement n'est effectué et l'itérateur ne renvoie que la valeur courante
  * @param[in] buff pointeur vers le buffer fourni à fun
+ * @pre b != NULL
+ * @pre -1 <= l < nb_ligne && -1 <= c < nb_colonnes
  *
  * @return Pointeur vers l'itérateur
  */
 BaleIte *createBaleIte(Bale *b, int l, int c, fun_ite_bale fun, void *buff);
 
 /**
- * @author VALLAT ugo
- * @date 1/11/2023
+ * @date 5/11/2023
  * @brief Renvoie vrai si il reste des éléments à parcourir
  *
  * @param[in] ite Itérateur questionné
+ * @pre ite != NULL
  *
  * @return true si il reste des éléments, sinon false
  */
 bool baleIteHasNext(BaleIte *ite);
 
 /**
- * @author VALLAT ugo
- * @date 1/11/2023
+ * @date 5/11/2023
  * @brief Décale l'itérateur sur le prochain élément, renvoie sa valeur et réalise
  *  le traitement de fun sans modification du ballot
  *
  * @param[in] ite Itérateur à utiliser
+ * @pre ite != NULL
  * @pre baleIteHasNext(ite) == true
  *
  * @return Valeur courante
@@ -197,23 +201,24 @@ bool baleIteHasNext(BaleIte *ite);
 int baleIteNext(BaleIte *ite);
 
 /**
- * @author VALLAT ugo
- * @date 1/11/2023
+ * @date 5/11/2023
  * @brief Renvoie la valeur courrante
  *
  * @param[in] ite Itérateur à utiliser
- * @pre ite != initBaleIte(ite)
+ * @pre ite != NULL
+ * @pre Appel à next avant
  *
  * @return Valeur de l'élément de courant
  */
 int baleIteGetValue(BaleIte *ite);
 
 /**
- * @author VALLAT ugo
- * @date 1/11/2023
+ * @date 5/11/2023
  * @brief Supprime l'itérateur et libère la mémoire
  *
  * @param[in] ite pointeur vers l'itérateur
+ * @pre ite != NULL
+ * @pre *ite != NULL
  */
 void deleteBaleIte(ptrBaleIte *ite);
 
@@ -222,8 +227,7 @@ void deleteBaleIte(ptrBaleIte *ite);
 /*------------------------------------------------------------------*/
 
 /**
- * @author VALLAT ugo
- * @date 1/11/2023
+ * @date 5/11/2023
  * @brief Applique la fonction fun à tous les éléments spécifiés du ballot sans
  * les modifier
  *
@@ -233,88 +237,102 @@ void deleteBaleIte(ptrBaleIte *ite);
  * @param[in] fun Fonction à appliquer à chaque élément, si NULL aucun traitement
  * n'est effectué
  * @param[in] buff pointeur vers le buffer fourni à fun
+ * @pre b != NULL
+ * @pre -1 <= l < nb_ligne && -1 <= c < nb_colonnes
  */
 void baleMap(Bale *b, int l, int c, fun_ite_bale fun, void *buff);
 
 /**
- * @author VALLAT ugo
- * @date 1/11/2023
+ * @date 5/11/2023
  * @brief Renvoie la somme des élément positifs du ballot, d'une ligne ou d'une colonne
  *
  * @param[in] b Pointeur vers le ballot
  * @param[in] l Ligne cible (-1 pour toutes les lignes)
  * @param[in] c Colonne cible (-1 pour toutes les colonnes)
+ * @pre b != NULL
+ * @pre -1 <= l < nb_ligne && -1 <= c < nb_colonnes
  *
  * @return somme des éléments positifs
  */
 int baleSom(Bale *b, int l, int c);
 
 /**
- * @author VALLAT ugo
- * @date 1/11/2023
- * @brief Renvoie la plus grande valeur du ballot, d'une ligne ou d'une colonne
+ * @date 5/11/2023
+ * @brief Renvoie la/les plus grande valeur du ballot, d'une ligne ou d'une colonne
  *
- * @param b Pointeur vers le ballot
- * @param l Ligne cible (-1 pour toutes les lignes)
- * @param c Colonne cible (-1 pour toutes les colonnes)
- * @return Tableau d'entiers de taille 3 contenant [valeur_max, ligne, colonne]
- */
-int *baleMax(Bale *b, int l, int c);
+ * @param[in] b Pointeur vers le ballot
+ * @param[in] l Ligne cible (-1 pour toutes les lignes)
+ * @param[in] c Colonne cible (-1 pour toutes les colonnes)
+ * @pre b != NULL
+ * @pre -1 <= l < nb_ligne && -1 <= c < nb_colonnes
+ *
+ * @return Une liste générique de tableaux d'entiers de taille 3 contenant [valeur_max, ligne,
+ *colonne]
+ **/
+GenList *baleMax(Bale *b, int l, int c);
 
 /**
- * @author VALLAT ugo
- * @date 1/11/2023
- * @brief Renvoie la plus petite valeur du ballot, d'une ligne ou d'une colonne
+ * @date 5/11/2023
+ * @brief Renvoie la/les plus petite valeur du ballot, d'une ligne ou d'une colonne
  *
- * @param b Pointeur vers le ballot
- * @param l Ligne cible (-1 pour toutes les lignes)
- * @param c Colonne cible (-1 pour toutes les colonnes)
- * @return Tableau d'entiers de taille 3 contenant [valeur_min, ligne, colonne]
- */
-int *baleMin(Bale *b, int l, int c);
+ * @param[in] b Pointeur vers le ballot
+ * @param[in] l Ligne cible (-1 pour toutes les lignes)
+ * @param[in] c Colonne cible (-1 pour toutes les colonnes)
+ * @pre b != NULL
+ * @pre -1 <= l < nb_ligne && -1 <= c < nb_colonnes
+ *
+ * @return Une liste générique de tableaux d'entiers de taille 3 contenant [valeur_min, ligne,
+ *colonne]
+ **/
+GenList *baleMin(Bale *b, int l, int c);
 
 /**
- * @author VALLAT ugo
- * @date 1/11/2023
+ * @date 5/11/2023
  * @brief Définie le type des fonctions prise en paramêtre du filter
  *
  * @param[in] v Valeur de la case courante
  * @param[in] l Ligne courante
  * @param[in] c Colonne courante
+ * @param[in] buff Buffer
+ * @pre l < nb_ligne && c < nb_colonnes
+ *
  * @return true si l'élément est conservé, sinon false
  */
-typedef bool (*fun_filter)(int v, int l, int c);
+typedef bool (*fun_filter)(int v, unsigned int l, unsigned int c, void *buff);
 
 /**
- * @author VALLAT ugo
- * @date 1/11/2023
+ * @date 5/11/2023
  * @brief Filtre le ballot et renvoie une copie du ballot avec uniquement
  * les éléments tel que fun(elem) = true
  *
- * @param b Pointeur vers le ballot
- * @param fun Fonction de filtrage de type fun_filter
+ * @param[in] b Pointeur vers le ballot
+ * @param[in] fun Fonction de filtrage de type fun_filter
+ * @param[in] buff Buffer pour fun_filter
+ * @pre b != NULL
+ * @pre fun != NULL
+ *
  * @return Pointeur vers la copie filtrée
  */
-Bale *baleFilter(Bale *b, fun_filter fun);
+Bale *baleFilter(Bale *b, fun_filter fun, void *buff);
 
 /**
- * @author VALLAT ugo
- * @date 1/11/2023
+ * @date 5/11/2023
  * @brief Copie un ballot
  *
  * @param[in] b Pointeur vers le ballot source (à copier)
- * @pre: (l_src, c_src) <= (l_dest,c_dest)
+ * @pre b != NULL
+ *
  * @return Pointeur vers la copie
  */
 
 Bale *baleCopy(Bale *b);
 
 /**
- * @author VALLAT ugo
- * @date 1/11/2023
+ * @date 5/11/2023
  * @brief Afficher le ballot dans la sortie standard stdout
  *
- * @param b Ballot à afficher
+ * @param[in] b Ballot à afficher
+ * @pre b != NULL
  */
 void displayBale(Bale *b);
 
@@ -324,6 +342,13 @@ void displayBale(Bale *b);
 
 #ifdef DEBUG
 
+/**
+ * @date 5/11/2023
+ * @brief Affiche dans le logger toutes les informations sur le ballot
+ *
+ * @param[in]  b ballot à logger
+ * @pre b != NULL
+ */
 void printBaleLog(Bale *b);
 
 #endif
--- src/structure/matrix.h
+++ src/structure/matrix.h
@@ -16,6 +16,7 @@
 
 #ifndef __MATRIX_H__
 #define __MATRIX_H__
+#include "genericlist.h"
 #include <errno.h>
 #include <stdbool.h>
 #include <stdio.h>
@@ -29,8 +30,7 @@ typedef struct s_matrix Matrix;
 typedef Matrix *ptrMatrix;
 
 /**
- * @author VALLAT ugo
- * @date  1/11/2023
+ * @date  5/11/2023
  * @brief Crée une matrice de dimension fixe
  *
  * @param[in] nbl Nombre de lignes
@@ -42,16 +42,15 @@ typedef Matrix *ptrMatrix;
 Matrix *createMatrix(unsigned int nbl, unsigned int nbc, int default_value);
 
 /**
- * @author VALLAT ugo
- * @date  1/11/2023
+ * @date  5/11/2023
  * @brief Supprime la matrice et ses données et libère la mémoire
  *
  * @param[in] m matrice à supprimer
+ * @pre m != NULL
  */
 void deleteMatrix(ptrMatrix *m);
 
 /**
- * @author VALLAT ugo
  * @date  31/10/2023
  * @brief Positionne un élément dans la matrice à l'emplacement (l,c)
  *
@@ -60,12 +59,12 @@ void deleteMatrix(ptrMatrix *m);
  * @param[in] c Colonne de destination (l,c)
  * @param[in] v valeur à ajouter
  *
- * @pre (l,c) < (nb_lignes, nb_colonnes)
+ * @pre l < nb_lignes && c < nb_colonnes
+ * @pre m != NULL
  */
 void matrixSet(Matrix *m, unsigned int l, unsigned int c, int v);
 
 /**
- * @author VALLAT ugo
  * @date  31/10/2023
  * @brief Efface la valeur en position (l,c) et la remplace par la valeur default_value
  *
@@ -73,31 +72,32 @@ void matrixSet(Matrix *m, unsigned int l, unsigned int c, int v);
  * @param[in] l Ligne de la suppression (l,c)
  * @param[in] c Colonne de la suppression(l,c)
  *
- * @pre (l,c) < (nb_lignes, nb_colonnes)
+ * @pre l < nb_lignes && c < nb_colonnes
+ * @pre m != NULL
  */
 void matrixErase(Matrix *m, unsigned int l, unsigned int c);
 
 /**
- * @author VALLAT ugo
- * @date  1/11/2023
+ * @date  5/11/2023
  * @brief Renvoie la valeur à la position (l,c) dans la matrice
  *
  * @param[in] m Pointeur vers la matrice
  * @param[in] l Ligne de la valeur
  * @param[in] c Colonne de la valeur
  *
- * @pre (l,c) < (nb_lignes, nb_colonnes)
+ * @pre l < nb_lignes && c < nb_colonnes
+ * @pre m != NULL
  *
  * @return la valeur en position (l,c)
  */
 int matrixGet(Matrix *m, unsigned int l, unsigned int c);
 
 /**
- * @author VALLAT ugo
- * @date  1/11/2023
+ * @date  5/11/2023
  * @brief Renvoie les dimension de la matrice sous la forme [nb_lignes, nb_colonnes]
  *
  * @param[in] m Pointeur vers la matrice
+ * @pre m != NULL
  *
  * @return tableau d'entiers naturels de taille 2 : [nombre_lignes, nombre_colonnes]
  */
@@ -105,50 +105,50 @@ int matrixGet(Matrix *m, unsigned int l, unsigned int c);
 unsigned int *matrixShape(Matrix *m);
 
 /**
- * @author VALLAT ugo
- * @date  1/11/2023
+ * @date  5/11/2023
  * @brief Ajouter une ligne à la position l
  *
  * @param[in] m Pointeur vers l'adresse de la matrice
  * @param[in] l Position de la ligne à ajouter
  *
  * @pre l <= nb_lignes
+ * @pre m != NULL
  */
 void matrixInsertLine(Matrix *m, unsigned int l);
 
 /**
- * @author VALLAT ugo
- * @date  1/11/2023
+ * @date  5/11/2023
  * @brief Supprime la ligne l
  *
  * @param[in] m Pointeur vers l'adresse de la matrice
  * @param[in] l Position de la ligne à supprimer
  *
  * @pre l < nb_lignes
+ * @pre m != NULL
  */
 void matrixRemoveLine(Matrix *m, unsigned int l);
 
 /**
- * @author VALLAT ugo
- * @date  1/11/2023
+ * @date  5/11/2023
  * @brief Ajouter une ligne à la position c
  *
  * @param[in] m Pointeur vers l'adresse de la matrice
  * @param[in] l Position de la ligne à ajouter
  *
  * @pre c <= nb_colonnes
+ * @pre m != NULL
  */
 void matrixInsertColumn(Matrix *m, unsigned int c);
 
 /**
- * @author VALLAT ugo
- * @date  1/11/2023
+ * @date  5/11/2023
  * @brief Supprime la colonne c
  *
  * @param[in] m Pointeur vers l'adresse de la matrice
  * @param[in] l Position de la colonne à supprimer
  *
  * @pre c < nb_colonnes
+ * @pre m != NULL
  */
 void matrixRemoveColumn(Matrix *m, unsigned int c);
 
@@ -161,22 +161,21 @@ typedef struct s_matrixIte MatrixIte;
 typedef struct s_matrixIte *ptrMatrixIte;
 
 /**
- * @author VALLAT ugo
- * @date 1/11/2023
+ * @date 5/11/2023
  * @brief Définie le type des fonctions prise en paramêtre de l'itérateur
  *
  * @param[in] v Valeur de la case courante
  * @param[in] l Ligne courante
  * @param[in] c Colonne courante
  * @param[in] buff pointeur vers un buffer pouvant stocker un résultat
+ * @pre l < nb_lignes && c < nb_colonnes
  *
  * @return nouvelle valeur de la case
  **/
 typedef int (*fun_ite)(int v, unsigned int l, unsigned int c, void *buff);
 
 /**
- * @author VALLAT ugo
- * @date 1/11/2023
+ * @date 5/11/2023
  * @brief Crée un itérateur sur la matrice passée en entrée et le positionne
  * avant le premier élément
  *
@@ -186,6 +185,8 @@ typedef int (*fun_ite)(int v, unsigned int l, unsigned int c, void *buff);
  * @param[in] fun Fonction à appliquer à chaque élément, si NULL aucun traitement
  * n'est appliqué et la matrice n'est pas modifiée
  * @param[in] buff pointeur vers le buffer fourni à fun
+ * @pre m != NULL
+ * @pre  -1 <= l < nb_lignes && -1 <= c <= nb_colonnes
  *
  * @return pointeur vers l'itérateur
  *
@@ -196,49 +197,48 @@ typedef int (*fun_ite)(int v, unsigned int l, unsigned int c, void *buff);
 MatrixIte *createMatrixIte(Matrix *m, int l, int c, fun_ite fun, void *buff);
 
 /**
- * @author VALLAT ugo
- * @date 1/11/2023
+ * @date 5/11/2023
  * @brief Renvoie si il reste des éléments à parcourir
  *
  * @param[in] ite pointeur vers l'itérateur
+ * @pre ite != NULL
  *
  * @return true si il reste des éléments, sinon false
  **/
 bool matrixIteHasNext(MatrixIte *ite);
 
 /**
- * @author VALLAT ugo
- * @date 1/11/2023
+ * @date 5/11/2023
  * @brief Décale l'itérateur sur le prochain élément, renvoie sa valeur avant modification,
  * applique la fonction fun et remplace la valeur par l'élément de retour de fun
  *
  * @param[in] ite pointeur vers l'itérateur
- *
- * @pre doit avoir un prochain élément
+ * @pre ite != NULL
+ * @pre MatrixIteHasNext(ite) == true
  *
  * @return valeur avant modification
  */
 int matrixIteNext(MatrixIte *ite);
 
 /**
- * @author VALLAT ugo
- * @date 1/11/2023
+ * @date 5/11/2023
  * @brief Renvoie la valeur courrante après application de la fonction fun
  *
  * @param[in] ite pointeur vers l'itérateur
- *
- * @pre doit être sur un élément
+ * @pre Appel à next avant
+ * @pre ite != NULL
  *
  * @return élément courant
  */
 int matrixIteGetValue(MatrixIte *ite);
 
 /**
- * @author VALLAT ugo
- * @date 1/11/2023
+ * @date 5/11/2023
  * @brief Supprime l'itérateur et libère la mémoire
  *
  * @param[in] ite pointeur vers l'itérateur
+ * @pre ite != NULL
+ * @pre *ite != NULL
  **/
 void deleteMatrixIte(ptrMatrixIte *ite);
 
@@ -247,8 +247,7 @@ void deleteMatrixIte(ptrMatrixIte *ite);
 /*------------------------------------------------------------------*/
 
 /**
- * @author VALLAT ugo
- * @date 1/11/2023
+ * @date 5/11/2023
  * @brief Applique la fonction fun à tous les éléments spédifiés de la matrice
  *
  * @param[in] m Pointeur vers la matrice à parcourir
@@ -257,73 +256,78 @@ void deleteMatrixIte(ptrMatrixIte *ite);
  * @param[in] fun Fonction à appliquer à chaque élément, si NULL aucun traitement
  * n'est appliqué et la matrice n'est pas modifiée
  * @param[in] buff pointeur vers le buffer fourni à fun
+ * @pre m != NULL
+ * @pre  -1 <= l < nb_lignes && -1 <= c <= nb_colonnes
  **/
 void matrixMap(Matrix *m, int l, int c, fun_ite fun, void *buff);
 
 /**
- * @author VALLAT ugo
- * @date 1/11/2023
+ * @date 5/11/2023
  * @brief Renvoie la somme des élément de la matrice, d'une ligne ou d'une colonne
  *
  * @param[in] m Pointeur vers la matrice
  * @param[in] l Ligne cible (-1 pour toutes les lignes)
  * @param[in] c Colonne cible (-1 pour toutes les colonnes)
+ * @pre  -1 <= l < nb_lignes && -1 <= c <= nb_colonnes
+ * @pre m != NULL
  *
  * @return somme des éléments
  **/
 long int matrixSum(Matrix *m, int l, int c);
 
 /**
- * @author VALLAT ugo
- * @date 1/11/2023
- * @brief Renvoie la plus grande valeur de la matrice, d'une ligne ou d'une colonne
+ * @date 5/11/2023
+ * @brief Renvoie la/les plus grande valeur de la matrice, d'une ligne ou d'une colonne
  *
  * @param[in] m Pointeur vers la matrice
  * @param[in] l Ligne cible (-1 pour toutes les lignes)
  * @param[in] c Colonne cible (-1 pour toutes les colonnes)
+ * @pre  -1 <= l < nb_lignes && -1 <= c <= nb_colonnes
+ * @pre m != NULL
  *
- * @return Tableau d'entiers de taille 3 contenant [valeur_max, ligne, colonne], NULL
- * si la matrice est vide
+ * @return Une liste générique de tableaux d'entiers de taille 3 contenant [valeur_max, ligne,
+ *colonne]
  **/
-int *matrixMax(Matrix *m, int l, int c);
+GenList *matrixMax(Matrix *m, int l, int c);
 
 /**
- * @author VALLAT ugo
- * @date1/11/2023
- * @brief Renvoie la plus petite valeur de la matrice, d'une ligne ou d'une colonne
+ * @date5/11/2023
+ * @brief Renvoie la/les plus petite valeur de la matrice, d'une ligne ou d'une colonne
  *
  * @param[in] m Pointeur vers la matrice
  * @param[in] l Ligne cible (-1 pour toutes les lignes)
  * @param[in] c Colonne cible (-1 pour toutes les colonnes)
+ * @pre  -1 <= l < nb_lignes && -1 <= c <= nb_colonnes
+ * @pre m != NULL
  *
- * @return Tableau d'entiers de taille 3 contenant [valeur_min, ligne, colonne], NULL
- * si la matrice est vide
+ * @return Une liste générique de tableaux d'entiers de taille 3 contenant [valeur_min, ligne,
+ *colonne]
  **/
-int *matrixMin(Matrix *m, int l, int c);
+GenList *matrixMin(Matrix *m, int l, int c);
 
 /**
- * @author VALLAT ugo
- * @date 1/11/2023
+ * @date 5/11/2023
  * @brief Définie le type des fonctions prise en paramêtre du filter
  *
  * @param[in] v Valeur de la case courante
  * @param[in] l Ligne courante
  * @param[in] c Colonne courante
  * @param[in] buff Buffer utilisable par fun_filter
+ * @pre l < nb_lignes && c < nb_colonnes
  *
  * @return true si l'élément est conservé, sinon false
  **/
 typedef bool (*fun_filter)(int v, unsigned int l, unsigned int c, void *buff);
 
 /**
- * @author VALLAT ugo
- * @date 1/11/2023
+ * @date 5/11/2023
  * @brief Filtre la matrice et renvoie une copie de la matrice avec uniquement
  * les éléments tel que fun(elem) = true
  *
  * @param[in] m Pointeur vers la matrice
  * @param[in] fun Fonction de filtrage de type fun_filter
  * @param[in] buff Buffer utilisé par fun_filter
+ * @pre m != NULL
  * @pre fun != NULL
  *
  * @return Pointeur vers la copie filtrée
@@ -331,24 +335,23 @@ typedef bool (*fun_filter)(int v, unsigned int l, unsigned int c, void *buff);
 Matrix *matrixFilter(Matrix *m, fun_filter fun, void *buff);
 
 /**
- * @author VALLAT ugo
- * @date 1/11/2023
+ * @date 5/11/2023
  * @brief Copie une matrice
  *
  * @param[in] m Pointeur vers la matrice source (à copier)
+ * @pre m != NULL
  *
- * @pre (l_src, c_src) <= (l_dest,c_dest)
  * @return Pointeur vers la copie
  */
 
 Matrix *matrixCopy(Matrix *m);
 
 /**
- * @author VALLAT ugo
- * @date 1/11/2023
+ * @date 5/11/2023
  * @brief Afficher la matrice dans la sortie standard stdout
  *
  * @param[in] m Matrice à afficher
+ * @pre m != NULL
  */
 void displayMatrix(Matrix *m);
 
@@ -359,11 +362,11 @@ void displayMatrix(Matrix *m);
 #ifdef DEBUG
 
 /**
+ * @date 5/11/2023
  * @brief Affiche dans le logger toutes les informations sur la matrice
  *
  * @param[in]  m matrice à logger
- * @author VALLAT ugo
- * @date 1/11/2023
+ * @pre m != NULL
  */
 void printMatrixLog(Matrix *m);
 
