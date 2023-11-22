72e194bba5e89525c6023bec85080c1285c7ca23
Corentin <eritque@DESKTOP-4BHCV59>
2023-11-22 11:12:28 +0100

    interpreter.h ajoue du boolean has_log_file

--- src/interpreter.h
+++ src/interpreter.h
@@ -6,12 +6,14 @@
 #ifndef __INTERPRETER__H__
 #define __INTERPRETER__H__
 
+#include <stdbool.h>
+
 /**
  * @date 27/10/2023
  * @brief Modules de calcul du vaiqueur
  */
 typedef enum e_module {
-    UNI1, UNI2, MINIMAX, RANGEMENT, SCHULZE, JUGEMENT_MAJORITAIRE, ALL
+    UNI1=1, UNI2, MINIMAX, RANGEMENT, SCHULZE, JUGEMENT_MAJORITAIRE, ALL
 } Module;
 
 
@@ -20,17 +22,18 @@ typedef enum e_module {
  * @brief Types de fichier csv pouvant être renseigné par l'utilisateur
  */
 typedef enum e_file_type {
-    BALE, DUEL, JUDGMENT
+    BALE=1, DUEL, JUDGMENT
 } FileType;
 
 /**
- * @date 27/10/2023
+ * @date 19/11/2023
  * @brief Contient toutes les informations nécéssaires au calcul du vainqueur
  */
 typedef struct command_t {
     Module module;        /* Module appelé, cf enum Module */
     FileType file_type;   /* Type du fichier de vote, cf enum FileType */
     char *file_name;      /* Nom du fichier de vote */
+    bool has_log_file;    
     char *log_file;       /* Potentiel nom du fichier de log */
 } Command;
 
