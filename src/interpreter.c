1dd964cf3850636a5f9dd8a1f5bfb8f9a8761501
Corentin <eritque@DESKTOP-4BHCV59>
2023-11-22 12:44:44 +0100

    interpreter.c test existance fichier

--- src/interpreter.c
+++ src/interpreter.c
@@ -2,6 +2,8 @@
 #include <getopt.h>
 #include <string.h>
 #include "logger.h"
+#include <stdio.h>
+#include <unistd.h>
 #include "interpreter.h"
 
 //@autor Corentin
@@ -47,7 +49,7 @@ Command intreprete(int argc, char *argv[])
             }
             else
             {
-                exitl("interpreter", "StringToModule", "Les balises i j et d sont incompatible\n",2);
+                exitl("interpreter", "intrepreter", "Les balises i j et d sont incompatible\n",2);
             }
             break;
 
@@ -59,7 +61,7 @@ Command intreprete(int argc, char *argv[])
             }
             else
             {
-                exitl("interpreter", "StringToModule", "Les balises i j et d sont incompatible\n",2);
+                exitl("interpreter", "intrepreter", "Les balises i j et d sont incompatible\n",2);
             }
             break;
 
@@ -71,7 +73,7 @@ Command intreprete(int argc, char *argv[])
             }
             else
             {
-                exitl("interpreter", "StringToModule", "Les balises i j et d sont incompatible\n",2);
+                exitl("interpreter", "intrepreter", "Les balises i j et d sont incompatible\n",2);
             }
             break;
 
@@ -79,7 +81,7 @@ Command intreprete(int argc, char *argv[])
         if (!command.has_log_file){
             command.log_file = optarg;
         } else {
-            exitl("interpreter", "StringToModule", "il ne peut avoir qu'un fichier de log\n",2); // TO DO
+            exitl("interpreter", "intrepreter", "il ne peut avoir qu'un fichier de log\n",2); // TO DO
         }
             break;
 
@@ -87,32 +89,37 @@ Command intreprete(int argc, char *argv[])
         if (command.module == 0){
             command.module = stringToModule(optarg);
         } else {
-            exitl("interpreter", "StringToModule", "il ne peut avoir qu'une seul balise de module\n",2); // TO DO
+            exitl("interpreter", "intrepreter", "il ne peut avoir qu'une seul balise de module\n",2); // TO DO
         }
             break;
 
         case '?':
-            exitl("interpreter", "StringToModule", "balise non reconnu ou argument manquant\n",2); // TO DO
+            exitl("interpreter", "intrepreter", "balise non reconnu ou argument manquant\n",2); // TO DO
             break;
         }
     }
 
+    if (!access(command.file_name, F_OK) != -1) {
+        pexitl("interpreter", "intrepreter", "fichier d'entree inexistant\n",2);
+    }
+
+
     if(command.module == 0 ){
-        exitl("interpreter", "StringToModule", "la commande doit avoir un -m\n",1);
+        exitl("interpreter", "intrepreter", "la commande doit avoir un -m\n",1);
     }
 
     if(command.file_type == 0 ){
-        exitl("interpreter", "StringToModule", "la commande doit avoir un -i, -d ou -j\n",1);
+        exitl("interpreter", "intrepreter", "la commande doit avoir un -i, -d ou -j\n",1);
     }
 
 
     // if(command.module!=JUGEMENT_MAJORITAIRE && command.file_type == JUDGMENT ){
-    //     exitl("interpreter", "StringToModule", "la balise j ne peut etre utiliser que par pour un methode de jugement majoritaire\n",1);
+    //     exitl("interpreter", "intreprete", "la balise j ne peut etre utiliser que par pour un methode de jugement majoritaire\n",1);
     // }
     
 
     if((command.module==UNI1 || command.module==UNI2 || command.module==JUGEMENT_MAJORITAIRE) && command.file_type==BALE){
-        exitl("interpreter", "StringToModule", "les methodes uninominals et jugement majoritaire ne peuvent etre appeler avec la balise -i\n",1); // TO DO
+        exitl("interpreter", "intrepreter", "les methodes uninominals et jugement majoritaire ne peuvent etre appeler avec la balise -i\n",1); // TO DO
     }
 
 
