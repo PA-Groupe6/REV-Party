a72618f03f5ab116707cfa6d42dd623bf7141cb0
loferga <laforgemateo@gmail.com>
2023-12-02 17:23:56 +0100

    avancé sur le test interpréteur

--- test/test_interpreter.c
+++ test/test_interpreter.c
@@ -1,8 +1,14 @@
+#include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
+#include <unistd.h>
+#include <wait.h>
 #include "../src/interpreter.h"
+#include "../src/logger.h"
 #include "test_utils.h"
 
 #define printsb(msg) addLineStringBuilder(string_builder, msg)
+#define LOGGER_OUPTUT "test/ressource/unit/interpreter.log"
 
 StringBuilder* string_builder;
 int return_value;
@@ -11,25 +17,80 @@ void beforeAll() {
     string_builder = createStringBuilder();
 }
 
+void afterAll() {
+    remove(LOGGER_OUPTUT);
+}
+
 void afterEach() {
     emptyStringBuilder(string_builder);
 }
 
-void testInterprete() {
-    int argc;
+void printCommand(Command* cmd) {
+    char cmdPrint[256];
+    memset(cmd, 0, sizeof(cmdPrint));
+    sprintf(cmdPrint, "\tCommand:\n\t\tModule:%d\n\t\tFileType:%d\n\t\tfile_name:\"%s\"\n\t\thas_log_file:%d\n\t\tlog_file:\"%s\"\n"
+                                       , cmd->module,     cmd->file_type,    cmd->file_name,       cmd->has_log_file,   cmd->log_file
+        );
+    printsb(cmdPrint);
+}
+
+Command* try(int argc, char* argv[]) {
+    int tube[2];
+    if (pipe(tube) == -1) {
+        perror("pipe");
+        exit(EXIT_FAILURE);
+    }
+    switch (fork()) {
+        case -1:
+            perror("fork");
+            exit(EXIT_FAILURE);
+        case 0: /* fils */
+            close(tube[0]);
+            init_logger(LOGGER_OUPTUT);
+            Command result = interprete(argc, argv);
+            write(tube[1], &result, sizeof(Command));
+            exit(EXIT_SUCCESS);
+        default: /* père */
+            close(tube[1]);
+
+            int xcode;
+            Command* cmd = malloc(sizeof(Command));
+
+            wait(&xcode);
+            if (WEXITSTATUS(xcode) == 0) {
+                read(tube[0], cmd, sizeof(Command));
+            } else {
+                cmd = NULL;
+            }
+
+            close(tube[0]);
+            return cmd;
+    }
+}
+
+bool testInterprete() {
+    // ### constantes de test
     char* cmd = "interprete";
-    char* file = "test/ressource/unit/d1.csv";
+    // drapeaux
     char* dflag = "-d";
     char* iflag = "-i";
     char* jflag = "-j";
     char* oflag = "-o";
-    char* offlag = "test/ressource/unit/res.log";
+    char* mflag = "-m";
+    // donnée supplémentaire
+    char* bale_src_file = "test/ressource/unit/bale_1.csv";
+    char* duel_src_file = "test/ressource/unit/duel_1.csv";
+    char* destfile = "any"; // no-OP surs destination
 
-    printsb("\ntest sur \"interprete -d test/ressource/unit/d1.csv\"");
-    argc = 3;
-    char* argv_d[argc];
-
-    Command cmd = interprete(3, argv_d);
+    printsb("\ntest sur \"interprete -d test/ressource/unit/bale_1.csv\"");
+    char* argv1[] = {cmd, dflag, bale_src_file};
+    Command* cmd1 = try(3, argv1);
+    if (cmd1 != NULL) {
+        printsb("Command extracted:\n");
+        printCommand(cmd1);
+        return false;
+    }
+    return true;
 }
 
 void test_fun(bool(*f)(), int fnb, char* fname) {
@@ -47,5 +108,6 @@ int main() {
 
     test_fun(testInterprete, 1, "testInterprete");
 
+    afterAll();
     return return_value;
 }
