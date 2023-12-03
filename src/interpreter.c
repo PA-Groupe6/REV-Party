717d9d794ef9d6695bd17955957801e04358a12a
loferga <laforgemateo@gmail.com>
2023-12-03 17:44:38 +0100

    correction induites par tests
    déclaration statique d'une commande

--- src/interpreter.c
+++ src/interpreter.c
@@ -35,74 +35,77 @@ Module stringToModule(char *nom)
  * @date 02/12/2023
  */
 
-Command interprete(int argc, char *argv[])
+Command* interprete(int argc, char *argv[])
 {
-    Command command;
+    Command* command = malloc(sizeof(struct command_t));
     int c;
-    command.file_type = 0;
-    command.module = 0;
-    command.has_log_file = false;
-    command.file_name = malloc(MAX_FILE_NAME);
-    command.log_file = malloc(MAX_FILE_NAME);
+    memset(command, 0, sizeof(struct command_t));
+    command->file_name[0] = '\0';
+    command->log_file[0] = '\0';
     while ((c = getopt(argc, argv, "-i:-d:-j:-o:-m:")) != -1)
     {
-        printf("%c\n",c);
         switch (c)
         {
         case 'i':
-            if (command.file_type == 0)
+            if (command->file_type == 0)
             {
-                command.file_type = BALE;
-                strcpy(command.file_name,optarg);
+                command->file_type = BALE;
+                strncpy(command->file_name, optarg, MAX_FILE_NAME);
             }
             else
             {
+                free(command);
                 exitl("interpreter", "intrepreter", EINCMPTB, "Les balises i j et d sont incompatible\n");
             }
             break;
 
         case 'd':
-            if (command.file_type == 0)
+            if (command->file_type == 0)
             {
-                command.file_type = DUEL;
-                strcpy(command.file_name,optarg);
+                command->file_type = DUEL;
+                strncpy(command->file_name, optarg, MAX_FILE_NAME);
             }
             else
             {
+                free(command);
                 exitl("interpreter", "intrepreter", EINCMPTB, "Les balises i j et d sont incompatible\n");
             }
             break;
 
         case 'j':
-            if(command.file_type == 0)
+            if(command->file_type == 0)
             {
-                command.file_type = JUDGMENT;
-                strcpy(command.file_name,optarg);
+                command->file_type = JUDGMENT;
+                strncpy(command->file_name, optarg, MAX_FILE_NAME);
             }
             else
             {
+                free(command);
                 exitl("interpreter", "intrepreter", EINCMPTB, "Les balises i j et d sont incompatible\n");
             }
             break;
 
         case 'o':
-        if (!command.has_log_file){
-            strcpy(command.log_file,optarg);
+        if (!command->has_log_file){
+            strncpy(command->log_file, optarg, MAX_FILE_NAME);
+            command->has_log_file = true;
         } else {
+            free(command);
             exitl("interpreter", "intrepreter", EINCMPTB, "il ne peut avoir qu'un fichier de log\n");
         }
             break;
 
         case 'm':
-        if (command.module == 0){
-            command.module = stringToModule(optarg);
-            printf("%d\n",command.module);
+        if (command->module == 0){
+            command->module = stringToModule(optarg);
         } else {
+            free(command);
             exitl("interpreter", "intrepreter", EINCMPTB, "il ne peut avoir qu'une seul balise de module\n");
         }
             break;
 
         case '?':
+            free(command);
             exitl("interpreter", "intrepreter", EUNKWARG, "balise non reconnu ou argument manquant\n");
             break;
         }
@@ -110,19 +113,23 @@ Command interprete(int argc, char *argv[])
 
 
 
-    if(command.module == 0 ){
+    if(command->module == 0 ){
+        free(command);
         exitl("interpreter", "intrepreter", EMISSARG, "la commande doit avoir un -m\n");
     }
 
-    if(command.file_type == 0 ){
+    if(command->file_type == 0 ){
+        free(command);
         exitl("interpreter", "intrepreter", EMISSARG, "la commande doit avoir un -i, -d ou -j\n");
     }
 
-    if (access(command.file_name, F_OK) == -1) {
+    if (access(command->file_name, F_OK) == -1) {
+        free(command);
         exitl("interpreter", "intrepreter", EINVLARG, "fichier d'entree inexistant\n");
     }
 
-    if((command.module==UNI1 || command.module==UNI2 || command.module==JUGEMENT_MAJORITAIRE) && command.file_type==DUEL){
+    if((command->module==UNI1 || command->module==UNI2 || command->module==JUGEMENT_MAJORITAIRE) && command->file_type==DUEL){
+        free(command);
         exitl("interpreter", "intrepreter", EINCMPTB, "les methodes uninominals et jugement majoritaire ne peuvent etre appeler avec la balise -d\n");
     }
 
