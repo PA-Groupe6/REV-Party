b670e89fe616cbc57b56a02381a076b3d6cf7403
ugoCodePasBeaucoup <max.planck.bg@gmail.com>
2023-12-03 11:23:52 +0100

    test csvToDuel

--- test/utils/test_csv_reader.c
+++ test/utils/test_csv_reader.c
@@ -54,36 +54,82 @@ bool echecTest(char* msg) {
 
 bool testCsvToBale() {
 
+    printsb("test sur bale 1...");
     Bale* b1 = csvToBale("test/ressource/unit/bale_1.csv");
     displayBaleLog(b1);
     deleteBale(&b1);
+    printsb("\t - test passé");
 
+    printsb("test sur bale 2...");
     Bale* b2 = csvToBale("test/ressource/unit/bale_2.csv");
     displayBaleLog(b2);
     deleteBale(&b2);
+    printsb("\t - test passé");
 
+    printsb("test sur bale 3...");
     Bale* b3 = csvToBale("test/ressource/unit/bale_3.csv");
     displayBaleLog(b3);
     deleteBale(&b3);
+    printsb("\t - test passé");
 
+    printsb("test sur bale 4...");
     Bale* b4 = csvToBale("test/ressource/unit/bale_4.csv");
     displayBaleLog(b4);
     deleteBale(&b4);
+    printsb("\t - test passé");
 
+    printsb("test sur bale 5...");
     Bale* b5 = csvToBale("test/ressource/unit/bale_5.csv");
     displayBaleLog(b5);
     deleteBale(&b5);
+    printsb("\t - test passé");
 
+    printsb("test sur bale 6...");
     Bale* b6 = csvToBale("test/ressource/unit/bale_6.csv");
     displayBaleLog(b6);
     deleteBale(&b6);
+    printsb("\t - test passé");
 
+    printsb("test sur bale 7...");
     Bale* b7 = csvToBale("test/ressource/unit/bale_7.csv");
     displayBaleLog(b7);
     deleteBale(&b7);
+    printsb("\t - test passé");
 
     return true;
 
+}
+
+
+bool testCsvToDuel() {
+    Duel* d;
+
+    printsb("test sur duel 1...");
+    d = csvToDuel("test/ressource/unit/duel_1.csv");
+    displayDuelLog(d);
+    deleteDuel(&d);
+    printsb("\t - test passé");
+
+    printsb("test sur duel 2...");
+    d = csvToDuel("test/ressource/unit/duel_2.csv");
+    displayDuelLog(d);
+    deleteDuel(&d);
+    printsb("\t - test passé");
+
+    printsb("test sur duel 3...");
+    d = csvToDuel("test/ressource/unit/duel_3.csv");
+    displayDuelLog(d);
+    deleteDuel(&d);
+    printsb("\t - test passé");
+
+    printsb("test sur duel 4...");
+    d = csvToDuel("test/ressource/unit/duel_4.csv");
+    displayDuelLog(d);
+    deleteDuel(&d);
+    printsb("\t - test passé");
+
+
+    return true;
 }
 
 
@@ -109,6 +155,7 @@ int main() {
     beforeAll();
 
     test_fun(testCsvToBale, 1, "testCsvToBale");
+    test_fun(testCsvToDuel, 1, "testCsvToDuel");
 
 
     afterAll();
